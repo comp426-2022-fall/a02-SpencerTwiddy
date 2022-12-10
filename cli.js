1295fe2f8b5f925772cbb5836be21a18658ea3cf
Spencer Twiddy <sjt49@live.unc.edu>
2022-12-09 20:52:52 -0500

    process lib, help text, minimist parse

--- cli.js
+++ cli.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
+import minimist from 'minimist';
+import process from 'process';
+var options = minimist(process.argv.slice(2));
 console.log("helium");
+if (options.h) {
+	console.log("help me tho");
+	process.exit(0);
+}
+console.log("made it to the end");
